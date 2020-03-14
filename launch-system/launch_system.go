9b0899b2353426eec6bc506258ac055332527ac9
veluscekm <marco.veluscek@skytv.it>
2020-03-15 00:25:51 +0100

    Add gin gonig dependency

--- launch-system/launch_system.go
+++ launch-system/launch_system.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"fmt"
+	/*"fmt"
 	"github.com/mvelusce/go-design-patterns/launch-system/rocket"
-	"time"
+	"time"*/
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-	groundControlChannel := make(chan bool)
+	/*groundControlChannel := make(chan bool)
 	rocketChannel := make(chan string)
 
 	go rocket.LaunchRocket(groundControlChannel, rocketChannel)
@@ -23,5 +24,19 @@ func main() {
 			return
 		}
 		time.Sleep(100 * time.Millisecond)
-	}
+	}*/
+
+	r := setupRouter()
+	r.Run()
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	//r.Run() // listen and serve on 0.0.0.0:8080
+	return r
 }
